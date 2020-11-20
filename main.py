ca0f00dd1193c7f393c2ff5640988f4e1ee23646
YSRKEN <ysr.ken@gmail.com>
2020-11-21 08:49:34 +0900

    [add] データベース用のクラスを用意した

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask
 
+from service.i_database_service import IDataBaseService
+from service.sqlite_database_service import SqliteDataBaseService
+
 app = Flask(__name__)
+database: IDataBaseService = SqliteDataBaseService('database.db')
+reader:  = DataReader(database)
 
 
 @app.route('/')
