cf0612d48b33ee9f4ce251693d0a0a85513aad59
YSRKEN <ysr.ken@gmail.com>
2020-11-21 13:46:43 +0900

    [add] レンズ名の選択、カメラ名、焦点距離の選択に応じて、絞り値の選択も一覧を切り替えられるようにした

--- lens-comparison-tool-by-tdp/src/App.tsx
+++ lens-comparison-tool-by-tdp/src/App.tsx
@@ -18,6 +18,11 @@ interface Fli {
   name: string;
 }
 
+interface Api {
+  id: string;
+  name: string;
+}
+
 interface SelectOption {
   value: string;
   label: string;
@@ -30,6 +35,8 @@ const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
   const [cameraList, setCameraList] = useState<Camera[]>([]);
   const [cameraId, setCameraId] = useState('');
   const [fliList, setFliList] = useState<Fli[]>([]);
+  const [fliId, setFliId] = useState('');
+  const [apiList, setApiList] = useState<Api[]>([]);
 
   useEffect(() => {
     if (lensId === '') {
@@ -54,15 +61,34 @@ const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
   }, [lensId, cameraId]);
 
   useEffect(() => {
+    if (lensId === '' || cameraId === '' || fliId === '') {
+      return;
+    }
+
+    const init = async () => {
+      setApiList(await (await fetch(`${SERVER_URL}/lenses/${lensId}/cameras/${cameraId}/flies/${fliId}/apis`)).json());
+    };
+    init();
+  }, [lensId, cameraId, fliId]);
+
+  useEffect(() => {
     if (cameraList.length > 0 && cameraList.filter(c => c.id === cameraId).length === 0) {
       setCameraId(cameraList[0].id);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cameraList]);
 
+  useEffect(() => {
+    if (fliList.length > 0 && fliList.filter(c => c.id === fliId).length === 0) {
+      setFliId(fliList[0].id);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [fliList]);
+
   const lensList2: SelectOption[] = lensList.map(l => { return { value: l.id, label: l.name } });
   const cameraList2: SelectOption[] = cameraList.map(l => { return { value: l.id, label: l.name } });
   const fliList2: SelectOption[] = fliList.map(l => { return { value: l.id, label: l.name } });
+  const apiList2: SelectOption[] = apiList.map(l => { return { value: l.id, label: l.name } });
 
   const onChangeLens = (e: ValueType<SelectOption>) => {
     if (e !== null && e !== undefined) {
@@ -76,14 +102,19 @@ const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
     }
   };
 
-  return <div className="border" style={{ width: '24rem' }}>
+  const onChangeFli = (e: ValueType<SelectOption>) => {
+    if (e !== null && e !== undefined) {
+      setFliId((e as SelectOption).value);
+    }
+  };
+
+  return <div className="border w-50">
     <Form className="m-3">
       <Form.Group>
-        <Select options={lensList2} placeholder="レンズ名を入力"
-          onChange={onChangeLens} />
-        <Select options={cameraList2} placeholder="カメラ名を入力"
-          onChange={onChangeCamera} />
-        <Select options={fliList2} placeholder="焦点距離を入力" />
+        <Select options={lensList2} placeholder="レンズ名を入力" onChange={onChangeLens} />
+        <Select options={cameraList2} placeholder="カメラ名を入力" onChange={onChangeCamera} />
+        <Select options={fliList2} placeholder="焦点距離を入力" onChange={onChangeFli} />
+        <Select options={apiList2} placeholder="絞り値を入力" />
       </Form.Group>
     </Form>
   </div>
