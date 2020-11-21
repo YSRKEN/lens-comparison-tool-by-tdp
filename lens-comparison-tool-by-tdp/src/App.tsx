da2553e75519388e7146bc7fa6980092a9c8bc6d
YSRKEN <ysr.ken@gmail.com>
2020-11-21 13:40:33 +0900

    [add] レンズ名の選択、カメラ名の選択に応じて、焦点距離の選択も一覧を切り替えられるようにした

--- lens-comparison-tool-by-tdp/src/App.tsx
+++ lens-comparison-tool-by-tdp/src/App.tsx
@@ -13,6 +13,11 @@ interface Camera {
   name: string;
 }
 
+interface Fli {
+  id: string;
+  name: string;
+}
+
 interface SelectOption {
   value: string;
   label: string;
@@ -23,7 +28,8 @@ const SERVER_URL = 'http://localhost:5000';
 const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
   const [lensId, setLensId] = useState('');
   const [cameraList, setCameraList] = useState<Camera[]>([]);
-  const [loadingCameraFlg, setLoadingCameraFlg] = useState(false);
+  const [cameraId, setCameraId] = useState('');
+  const [fliList, setFliList] = useState<Fli[]>([]);
 
   useEffect(() => {
     if (lensId === '') {
@@ -32,18 +38,41 @@ const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
 
     const init = async () => {
       setCameraList(await (await fetch(`${SERVER_URL}/lenses/${lensId}/cameras`)).json());
-      setLoadingCameraFlg(false);
     };
     init();
   }, [lensId]);
 
+  useEffect(() => {
+    if (lensId === '' || cameraId === '') {
+      return;
+    }
+
+    const init = async () => {
+      setFliList(await (await fetch(`${SERVER_URL}/lenses/${lensId}/cameras/${cameraId}/flies`)).json());
+    };
+    init();
+  }, [lensId, cameraId]);
+
+  useEffect(() => {
+    if (cameraList.length > 0 && cameraList.filter(c => c.id === cameraId).length === 0) {
+      setCameraId(cameraList[0].id);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cameraList]);
+
   const lensList2: SelectOption[] = lensList.map(l => { return { value: l.id, label: l.name } });
   const cameraList2: SelectOption[] = cameraList.map(l => { return { value: l.id, label: l.name } });
+  const fliList2: SelectOption[] = fliList.map(l => { return { value: l.id, label: l.name } });
 
   const onChangeLens = (e: ValueType<SelectOption>) => {
     if (e !== null && e !== undefined) {
       setLensId((e as SelectOption).value);
-      setLoadingCameraFlg(true);
+    }
+  };
+
+  const onChangeCamera = (e: ValueType<SelectOption>) => {
+    if (e !== null && e !== undefined) {
+      setCameraId((e as SelectOption).value);
     }
   };
 
@@ -53,7 +82,8 @@ const LensDataCard: React.FC<{ lensList: Lens[] }> = ({ lensList }) => {
         <Select options={lensList2} placeholder="レンズ名を入力"
           onChange={onChangeLens} />
         <Select options={cameraList2} placeholder="カメラ名を入力"
-          isDisabled={loadingCameraFlg} />
+          onChange={onChangeCamera} />
+        <Select options={fliList2} placeholder="焦点距離を入力" />
       </Form.Group>
     </Form>
   </div>
