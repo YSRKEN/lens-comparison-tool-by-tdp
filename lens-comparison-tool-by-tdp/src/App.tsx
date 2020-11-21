a394c16acc8ea32b9f6fe948497376819e95169f
YSRKEN <ysr.ken@gmail.com>
2020-11-21 12:24:44 +0900

    [add] React Bootstrapを導入した

--- lens-comparison-tool-by-tdp/src/App.tsx
+++ lens-comparison-tool-by-tdp/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const App: React.FC = () => {
-  return (<span>test</span>);
+  return <Container>
+    <Row className="my-3">
+      <Col className="text-center">
+        <h1>レンズ比較ツール</h1>
+      </Col>
+    </Row>
+  </Container>;
 }
 
 export default App;
