39e3a18eb3035b67a74be32014db67cf2fc68984
YSRKEN <ysr.ken@gmail.com>
2020-12-04 18:48:27 +0900

    [add] ドキュメントに追記した

--- lens-comparison-tool-by-tdp/src/App.tsx
+++ lens-comparison-tool-by-tdp/src/App.tsx
@@ -1,14 +1,19 @@
 import React, { } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-import LensDataCard from 'component/LensDataCard';
 import LensDataCard2 from 'component/LensDataCard2';
-import { AppContext, useAppStore } from 'service/store';
 
 const App: React.FC = () => {
   return <Container fluid>
     <Row className="my-3">
       <Col className="text-center">
         <h1>レンズ比較ツール</h1>
+      </Col>
+    </Row>
+    <Row className="my-3">
+      <Col className="text-center">
+        <span className="d-inline-block mr-3">Ver.1.0.0</span>
+        <span className="d-inline-block mr-3"><a href="https://github.com/YSRKEN/lens-comparison-tool-by-tdp" rel="noreferrer" target="_blank">GitHub</a></span>
+        <span><a href="https://twitter.com/YSRKEN" rel="noreferrer" target="_blank">ツール作者のTwitter</a></span>
       </Col>
     </Row>
     <Row className="my-3">
