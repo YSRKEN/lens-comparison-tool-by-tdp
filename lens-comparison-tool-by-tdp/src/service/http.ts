95fc5bf21bb2f4c564183993dc88841a331d0615
YSRKEN <ysr.ken@gmail.com>
2020-12-04 17:59:26 +0900

    [add] データキャッシュをローカルストレージにも蓄えるようにした

--- lens-comparison-tool-by-tdp/src/service/http.ts
+++ lens-comparison-tool-by-tdp/src/service/http.ts
@@ -1,6 +1,10 @@
 import { SERVER_URL } from "constant/other";
 
-const cache: { [key: string]: any } = {};
+let cache: { [key: string]: any } = {};
+const temp = window.localStorage.getItem('httpCache');
+if (temp !== null) {
+  cache = JSON.parse(temp);
+}
 
 export const getData = async <T>(path: string): Promise<T> => {
   if (path in cache) {
@@ -8,6 +12,7 @@ export const getData = async <T>(path: string): Promise<T> => {
   } else {
     const data: T = await (await fetch(`${SERVER_URL}${path}`)).json();
     cache[path] = data;
+    window.localStorage.setItem('httpCache', JSON.stringify(cache));
     return data;
   }
 };
